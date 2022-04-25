0c51c38822c4ef7b965f61d40bef13d2e073d1a1
scottmangles <scottmangles@hotmail.com>
2022-04-25 14:35:43 +0100

    login page
    login page part edited

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,155 @@
+<x-guest-layout>
+    
+
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-white">
+  <body class="h-full">
+  ```
+-->
+ <!-- Validation Errors -->
+ <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<div class="min-h-full flex">
+    
+    <div class="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div>
+          <img class="h-12 w-auto" src="https://www.moduspartnerships.co.uk/assets/img/logo-dark.png" alt="Workflow">
+          <h2 class="mt-6 text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+        </div>
+  
+        <div class="mt-8">
+  
+          <div class="mt-6">
+            <form action="{{ route('login') }}" method="POST" class="space-y-6">
+            @csrf
+               <!-- Email Address -->
+        <div>
+            <x-label for="email" :value="__('Email')" />
+
+            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-label for="password" :value="__('Password')" />
+
+            <x-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+        </div>
+
+        
+  
+              <div class="flex items-center justify-between">
+                   <!-- Remember Me -->
+         <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-[#173a68] shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+                
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+            </div>
+                <div class="text-sm">
+                  <a href="#" class="font-medium text-[#173a68] hover:text-blue-800"> Forgot your password? </a>
+                </div>
+              </div>
+  
+              <div>
+                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#173a68] hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Sign in</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hidden lg:block relative w-0 flex-1">
+      <img class="absolute inset-0 h-full w-full object-cover" src="https://www.moduspartnerships.co.uk/assets/img/slider/slider1a.jpg" alt="">
+      <img class="absolute inset-0" src="https://www.moduspartnerships.co.uk/assets/img/logo-dark.png" alt="">
+    </div>
+  </div>
+
+</x-guest-layout>
+
+
+
+  <x-auth-card>
+    <x-slot name="logo">
+        <a href="/">
+            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+        </a>
+    </x-slot>
+
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <!-- Validation Errors -->
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-label for="email" :value="__('Email')" />
+
+            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-label for="password" :value="__('Password')" />
+
+            <x-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-blue-600 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-button class="ml-3">
+                {{ __('Log in') }}
+            </x-button>
+        </div>
+    </form>
+</x-auth-card>
