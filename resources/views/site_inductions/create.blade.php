d736682d99fdfb87142f5a5712507a3aec403289
scottmangles <scottmangles@hotmail.com>
2022-05-06 19:27:48 +0100

    web pack bug fix

--- resources/views/site_inductions/create.blade.php
+++ resources/views/site_inductions/create.blade.php
@@ -5,7 +5,8 @@
             {{ __('Site Induction') }}
         </h2>
     </x-slot>
-<br>
-    video
-
+    <div class="mt-3">
+        <img  class="mx-auto" src="{{ URL::asset('images/play-thumb.png') }}">
+    </div>
+    <button type="button" class="inline-flex items-center px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Button text</button>
 </x-app-layout>
