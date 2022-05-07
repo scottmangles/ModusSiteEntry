df4205acd2fbb14fe405c38d4b4c81b93fcafdaa
scottmangles <scottmangles@hotmail.com>
2022-05-07 18:36:55 +0100

    Update create.blade.php

--- resources/views/site_inductions/create.blade.php
+++ resources/views/site_inductions/create.blade.php
@@ -6,7 +6,7 @@
         </h2>
     </x-slot>
     <div class="mt-3">
-        <img  class="mx-auto" src="{{ URL::asset('images/play-thumb.png') }}">
+        <img  class="mx-auto" src="{{ URL::asset('images/play-thumb.png') }}"> f
     </div>
     <button type="button" class="inline-flex items-center px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Button text</button>
 </x-app-layout>
