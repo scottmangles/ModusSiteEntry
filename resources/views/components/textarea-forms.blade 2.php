90419bd8a77976f025a398245c9c594bc699aea5
scottmangles <scottmangles@hotmail.com>
2022-06-07 14:37:16 +0100

    more duplicates deleted

--- resources/views/components/textarea-forms.blade 2.php
+++
@@ -1,5 +0,0 @@
-@props(['disabled' => false])
-
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block max-w-lg w-full shadow-sm focus:ring-[#173a68] focus:border-[#173a68] sm:text-sm border-gray-300 rounded-md']) !!}>
-{{ $slot }}
-</textarea>
