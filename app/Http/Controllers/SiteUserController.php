792b72bbbb7408789c159a843c40c18f2876c706
scottmangles <scottmangles@hotmail.com>
2022-06-16 15:40:21 +0100

    Update SiteUserController.php

--- app/Http/Controllers/SiteUserController.php
+++ app/Http/Controllers/SiteUserController.php
@@ -112,7 +112,7 @@ class SiteUserController extends Controller
                 
            // dd($checkEntryStatus);
 
-            if ($checkEntryStatus == 'access denied' or $checkEntryStatus == null) {
+            if ($checkEntryStatus->status == 'access denied' or $checkEntryStatus == null) {
                 // return warning not granted access by site manager
                 return redirect()
                 ->route('dashboard')
