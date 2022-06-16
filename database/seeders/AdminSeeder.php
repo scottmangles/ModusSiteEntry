1535b946e4f2a1ea4d02de2fe43c7bb0aba87b08
scottmangles <scottmangles@hotmail.com>
2022-06-16 14:44:43 +0100

    Create AdminSeeder.php

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User([
+            'name' => 'Scott Mangles',
+            'email' => 'admin@smathletic.co.uk',
+            'email_verified_at' => now(),
+            'role' => 'admin',
+            'password' => Hash::make('SMAthletic1!'),
+            'mobile' => '07538400748',
+            'sub_contractor' => 1,
+            'vehicle_make' => 'Saab',
+            'vehicle_reg' => 'LB06 VLD',
+            'cscs_number' => NULL,
+            'remember_token' => Str::random(10)
+        ]);
+
+        $admin->save();
+    }
+}
