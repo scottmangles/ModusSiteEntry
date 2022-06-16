2af8d5b081243adbbc091b3a2123864df2ba6778
scottmangles <scottmangles@hotmail.com>
2022-06-16 14:45:22 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Contractor::factory(10)->create();
-        \App\Models\User::factory(20)->create();
-        \App\Models\Site::factory(10)->create();
+      //  \App\Models\Contractor::factory(10)->create();
+       // \App\Models\User::factory(20)->create();
+       // \App\Models\Site::factory(10)->create();
     }
 }
