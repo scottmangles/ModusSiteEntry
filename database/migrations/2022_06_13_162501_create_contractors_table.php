d31ea51c4931283f7052cbab5e55130bef209302
Shift <shift@laravelshift.com>
2022-07-13 19:24:48 +0000

    Adopt anonymous migrations

--- database/migrations/2022_06_13_162501_create_contractors_table.php
+++ database/migrations/2022_06_13_162501_create_contractors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContractorsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class CreateContractorsTable extends Migration
     {
         Schema::dropIfExists('contractors');
     }
-}
+};
